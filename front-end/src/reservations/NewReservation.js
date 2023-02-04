8343b71d493fe8ec3f294978dde07ab8b3fba8a4
Adrian Powers <adrianpowers530@gmail.com>
2023-02-04 00:08:12 -0500

    seeds and migrations working

---
+++ front-end/src/reservations/NewReservation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "../layout/Menu";
+
+function NewReservation() {
+  return (
+    <div className="container-fluid">
+      <div className="row h-100">
+        <div className="col-md-2 side-bar">
+          <Menu />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewReservation();
